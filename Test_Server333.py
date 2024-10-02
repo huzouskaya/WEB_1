e6cd66c3f76a8787e891566cb86959925b65208b
huzouskaya <huzouskaya.aliaksandra@gmail.com>
2024-10-02 11:39:47 +1000

    Add files via upload

--- Test_Server333.py
+++ Test_Server333.py
@@ -4,26 +4,23 @@ import socket
 import threading
 import queue
 
-# Create a queue to store the received data
 data_queue = queue.Queue()
 
 class Checker:
     def __init__(self, color, x, y):
-        self.color = color  # Цвет шашки ('red' или 'black')
-        self.x = x  # Координата клетки по оси X
-        self.y = y  # Координата клетки по оси Y
-        self.tile_size = 80  # Размер клетки доски
+        self.color = color
+        self.x = x
+        self.y = y
+        self.tile_size = 80
 
     def draw(self, screen):
-        # Рисуем шашку на экране
-        color = (255, 0, 0) if self.color == 'red' else (255, 255, 255)
+        color = (0, 0, 0) if self.color == 'black' else (255, 255, 255)
         pygame.draw.circle(screen, color, (self.x * self.tile_size + self.tile_size // 2,
                                              self.y * self.tile_size + self.tile_size // 2),
                            self.tile_size // 3)
 
     def can_move(self, target_x, target_y):
-        # Логика движения шашки. Например, шашка может двигаться на одну клетку по диагонали
-        if self.color != 'red':
+        if self.color != 'black':
             return (target_x == self.x + 1 and target_y == self.y + 1) or (target_x == self.x - 1 and target_y == self.y + 1)
         else:
             return (target_x == self.x + 1 and target_y == self.y - 1) or (target_x == self.x - 1 and target_y == self.y - 1)
@@ -32,18 +29,18 @@ class Game:
     def __init__(self):
         self.checkers = []
         for x in range(8):
-            for y in range(3):  # Верхние 3 строки для красных шашек
-                if (x + y) % 2 == 1:  # Расставляем только на черных клетках
-                    self.checkers.append(Checker('red', x, y))
+            for y in range(3):
+                if (x + y) % 2 == 1:
+                    self.checkers.append(Checker('black', x, y))
 
         for x in range(8):
-            for y in range(5, 8):  # Нижние 3 строки для черных шашек
+            for y in range(5, 8):
                 if (x + y) % 2 == 1:
-                    self.checkers.append(Checker('wight', x, y))
+                    self.checkers.append(Checker('white', x, y))
 
     def start_server(self):
         server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        server.bind(('192.168.146.136', 12345))
+        server.bind(('127.0.0.1', 12345))
         server.listen(1)
         print("Сервер запущен. Ожидание подключения...")
 
@@ -73,7 +70,7 @@ class Game:
         screen = pygame.display.set_mode((WIDTH, HEIGHT))
         pygame.display.set_caption("Шашки")
 
-        colors = [pygame.Color(255, 255, 255), pygame.Color(0, 0, 0)]
+        colors = [pygame.Color(255, 222, 153), pygame.Color(139, 69, 19)]
 
         while True:
             for event in pygame.event.get():
@@ -81,7 +78,7 @@ class Game:
                     pygame.quit()
                     sys.exit()
 
-            screen.fill((255, 255, 255))  # Очистка экрана
+            screen.fill((255, 255, 255))
 
             for row in range(8):
                 for col in range(8):
@@ -91,8 +88,9 @@ class Game:
             for checker in self.checkers:
                 checker.draw(screen)
 
-            pygame.display.flip()# Обновление экрана
-            pygame.time.Clock().tick(60)  # Ограничение частоты кадров
+            pygame.display.flip()
+            pygame.time.Clock().tick(60)
+
 
 if __name__ == "__main__":
     game = Game()
